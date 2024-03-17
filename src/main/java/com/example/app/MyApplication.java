361427711b06bf8400a5e222fefdb9640bde0b4c
yashK98 <yash.4198@gmail.com>
2024-03-17 15:57:51 +0530

    Initial Commit

---
+++ src/main/java/com/example/app/MyApplication.java
@@ -0,0 +1,14 @@
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class MyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/app/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.app.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String getMessage(){
+        return "Welcome to Spring";
+    }
+}
